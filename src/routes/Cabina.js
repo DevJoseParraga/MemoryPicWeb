bd6fb4d1a2311e604b92a87b723c43e6b9c7a58a
DevJoseParraga <DevJoseParraga@gmail.com>
2023-11-14 20:56:56 -0300

    Cambiando formato de imgs

--- src/routes/Cabina.js
+++ src/routes/Cabina.js
@@ -121,9 +121,9 @@ function Cabina() {
                 <img className='fotosImg1 fotosNone fotos' src='./_DSC3430.jpg' alt='foto'></img>
               </div>
               <div className='fotosContainer2'>
-                <img className='fotosImg2 fotos' src='./IMG_0834.jpg' alt='foto'></img>
-                <img className='fotosImg2 fotos' src='./IMG_3747.jpg' alt='foto'></img>
-                <img className='fotosImg2 fotosNone fotos' src='./IMG_7481.jpg' alt='foto'></img>
+                <img className='fotosImg2 fotos' src='./IMG_2856.jpg' alt='foto'></img>
+                <img className='fotosImg2 fotos' src='./IMG_2857.jpg' alt='foto'></img>
+                <img className='fotosImg2 fotosNone fotos' src='./C20B419A-657B-4465-A321-391DCF0D9828.jpg' alt='foto'></img>
               </div>
               <div className='fotosContainer3'>
                 <img className='fotosImg3 fotos' src='./DSC_0123.jpg' alt='foto'></img>
